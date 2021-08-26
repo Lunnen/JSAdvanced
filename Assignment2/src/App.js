cecd7e190869350982c8d2d6808394638a09c085
Andreas <69907843+Lunnen@users.noreply.github.com>
2021-08-26 09:11:01 +0200

    minor CSS and PromiseAll
    - CSS fixes in layout
    - PromiseAll for multi-fetch instead of the classic way

--- Assignment2/src/App.js
+++ Assignment2/src/App.js
@@ -3,67 +3,57 @@ import RingLoader from "react-spinners/RingLoader";
 import CardBox from "./components/CardBox";
 
 const App = () => {
-
   const [showThis, setShowThis] = useState([]);
   const [loading, setLoading] = useState(true);
 
-  const loaderWaitTime = 1500;
+  const loaderWaitTime = 1200;
+  const serverURL = "https://swapi.py4e.com/api/films";
 
   useEffect(() => {
     async function fetchData() {
-      let res = await fetch('https://swapi.py4e.com/api/films');
+      let res = await fetch(serverURL);
       let data = await res.json();
 
       if (data) {
         setShowThis(data.results);
-        
+
         // Sort movies by release_date
         setShowThis((currentList) => [
-          ...currentList.sort((a, b) => a.release_date - b.release_date)
+          ...currentList.sort((a, b) => a.release_date - b.release_date),
         ]);
 
         setLoading(false);
       }
-    
     }
 
     setTimeout(() => {
-    fetchData();
+      fetchData();
     }, loaderWaitTime); // Just to show Loading screen
   }, []); // Loading the page, get info once
-  
+
   return (
     <div className="main-container">
-
       {loading && (
-        <div style={{ margin: "5rem"}}>
-          <RingLoader
-            color={"#fff850"}
-            size={125}
-          
-          />
+        <div style={{ margin: "5rem" }}>
+          <RingLoader color={"#fff850"} size={125} />
           <h2 style={{ color: "#fff850", margin: "50% 20%" }}>Loading...</h2>
         </div>
       )}
 
-      {!loading && (  
-
-      <div className="all-container">
-
-       {showThis.map((Stats, index) => (
-          <CardBox
-            key={index}
-            episode_id={Stats.episode_id}
-            title={Stats.title}
-            release_date={Stats.release_date}
-            characters={Stats.characters}
-          /> 
-        ))}
-
-      </div>
-         )}
-    
-  
+      {!loading && (
+        <div className="all-container">
+          {showThis.map((Stats, index) => (
+            <CardBox
+              key={index}
+              title={Stats.title}
+              release_date={Stats.release_date}
+              characters={Stats.characters}
+              opening_crawl={Stats.opening_crawl}
+              setLoading={setLoading}
+            />
+          ))}
+        </div>
+      )}
     </div>
   );
 };
--- Assignment2/src/components/CardBox.js
+++ Assignment2/src/components/CardBox.js
@@ -1,60 +1,66 @@
 import React, { useState, useEffect } from "react";
 
-const CardBox = (CardBox) => {
-
+const CardBox = ({
+  title,
+  release_date,
+  characters,
+  opening_crawl,
+  setLoading,
+}) => {
   const [modal, setModal] = useState(false);
 
-  const [currentActors, setCurrentActors] = useState([]);
+  const [currentActors] = useState([]);
 
   const modalOnOff = () => setModal(!modal);
 
-  if(modal ? document.body.classList.add('active-modal') : document.body.classList.remove('active-modal') );
+  if (
+    modal
+      ? document.body.classList.add("active-modal")
+      : document.body.classList.remove("active-modal")
+  );
 
   useEffect(() => {
-   
-    async function fetchDataThis(specificUrl) {
-      let res = await fetch(`${specificUrl}`);
-      let data = await res.json();
+    setLoading(true);
 
-      if (data) {
-        currentActors.push(data);
-        currentActors.sort((a, b) => a.name.localeCompare(b.name));
-      }
-    }
-    {CardBox.characters.map( (item) => fetchDataThis(item) )};
+    Promise.all(
+      characters.map((actor) =>
+        fetch(actor)
+          .then((response) => response.json())
+          .then((responseBody) => {
+            currentActors.push(responseBody);
+            currentActors.sort((a, b) => a.name.localeCompare(b.name));
+          })
+      )
+    );
 
+    setLoading(false);
   }, []); // Loading the page, get info once
 
-  return(
+  return (
     <div>
-    {!modal && (
-    <div className="thumb-container" onClick={modalOnOff}>
- 
-        <h3>{CardBox.title}</h3>
+      {!modal && (
+        <div className="thumb-container" onClick={modalOnOff}>
+          <h3>{title}</h3>
 
-        <h4>{CardBox.release_date}</h4>
+          <h4>{release_date}</h4>
+        </div>
+      )}
+      {modal && (
+        <div className="thumb-container overlay" onClick={modalOnOff}>
+          <h1>{title}</h1>
+          <h3>{release_date}</h3>
+          <h6 className="crawl-container">{opening_crawl}</h6>
 
+          <ol className="actorBox">
+            <h2>Actors:</h2>
+            {currentActors.map((item, index) => (
+              <li key={index}>{item.name}</li>
+            ))}
+          </ol>
+        </div>
+      )}
     </div>
-  )}
-  {modal && (
-    <div className="thumb-container overlay" onClick={modalOnOff}>
-
-        <h1>{CardBox.title}</h1>
-        <h3>{CardBox.release_date}</h3>
-
-        <ol className="actorBox">
-          
-          {currentActors.map((item, index) =>
-          <li key={index}>{item.name}</li>
-          )}
-
-        </ol>
-
-    </div>
-  )}
-
-  </div>
   );
-}
+};
 
 export default CardBox;
